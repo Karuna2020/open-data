9387d70381bd30eaf7cad7312026c66e6f3779ff
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-28 17:43:21 +0530

    :heavy_plus_sign: Add @types/fs-extra

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
 import got from "got";
 import { join } from "path";
-import { readFile } from "fs-extra";
+import { readFile, writeJson } from "fs-extra";
 import { safeLoad } from "js-yaml";
 
 const fetchData = async () => {
   const yaml = await readFile(join(".", "sheet.yml"), "utf8");
   const sheetFile: { publicEndpoint: string; tabs: string[] } = safeLoad(yaml);
-  console.log(sheetFile);
 
-  // const { body } = await got.get<{ [index: string]: string }[]>(
-  //   SHEETDB_PUBLIC_API_ENDPOINT,
-  //   {
-  //     responseType: "json"
-  //   }
-  // );
+  for await (const tab of sheetFile.tabs) {
+    const { body } = await got.get<{ [index: string]: string }[]>(
+      `${sheetFile.publicEndpoint}?sheet=11.%20IT/%20Website`,
+      {
+        responseType: "json"
+      }
+    );
+    await writeJson(join(), body);
+  }
 };
 
 fetchData();
