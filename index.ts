782a4ce9bd18996c6b8f43c093d568e0d12e3d1b
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-28 17:54:06 +0530

    :sparkles: Log and write data files

--- index.ts
+++ index.ts
@@ -4,21 +4,30 @@ import { readFile, writeJson } from "fs-extra";
 import { safeLoad } from "js-yaml";
 import slugify from "@sindresorhus/slugify";
 
+const log = (...args: string[]) =>
+  console.log(new Date().toISOString(), ...args);
+
 const fetchData = async () => {
   const yaml = await readFile(join(".", "sheet.yml"), "utf8");
   const sheetFile: { publicEndpoint: string; tabs: string[] } = safeLoad(yaml);
 
   for await (const tab of sheetFile.tabs) {
-    const { body } = await got.get<{ [index: string]: string }[]>(
-      `${sheetFile.publicEndpoint}?sheet=${encodeURIComponent(tab)}`,
-      {
-        responseType: "json"
-      }
-    );
-    await writeJson(
-      join(".", `${slugify(tab.replace(/\d\.\s+/g, ""))}.json`),
-      body
-    );
+    log("Updated data file", tab);
+    try {
+      const { body } = await got.get<{ [index: string]: string }[]>(
+        `${sheetFile.publicEndpoint}?sheet=${encodeURIComponent(tab)}`,
+        {
+          responseType: "json"
+        }
+      );
+      await writeJson(
+        join(".", `${slugify(tab.replace(/\d\.\s+/g, ""))}.json`),
+        body
+      );
+      log("SUCCESS", tab);
+    } catch (error) {
+      log("ERROR", tab, error);
+    }
   }
 };
 
