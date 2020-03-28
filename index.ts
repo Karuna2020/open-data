d9ecaafcf6c2211071bcf4d53016d9b895f02b0d
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-28 17:41:02 +0530

    :sparkles: Add public endpoint to YAML

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import got from "got";
+import { join } from "path";
+import { readFile } from "fs-extra";
 import { safeLoad } from "js-yaml";
 
-const SHEETDB_PUBLIC_API_ENDPOINT = `https://sheetdb.io/api/v1/qnigd524si9dw`;
-
 const fetchData = async () => {
+  const yaml = await readFile(join(".", "sheet.yml"), "utf8");
+  const sheetFile: { publicEndpoint: string; tabs: string[] } = safeLoad(yaml);
+  console.log(sheetFile);
+
   // const { body } = await got.get<{ [index: string]: string }[]>(
   //   SHEETDB_PUBLIC_API_ENDPOINT,
   //   {
