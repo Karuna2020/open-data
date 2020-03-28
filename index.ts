43f0955f2413547bb062b74549ced0ca77c0256e
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-28 17:34:44 +0530

    :heavy_plus_sign: Add typescript, ts-node

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
+import got from "got";
+
 const SHEETDB_PUBLIC_API_ENDPOINT = `https://sheetdb.io/api/v1/qnigd524si9dw`;
+
+const fetchData = async () => {
+  const { body } = await got.get<{}>(SHEETDB_PUBLIC_API_ENDPOINT, {
+    responseType: "json"
+  });
+  console.log(JSON.stringify(body));
+};
