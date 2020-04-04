afd4df5e17c3549cd32f675ea97722c6cc01c6f0
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-04 22:36:45 +0530

    :construction: Set up Airtable API

--- airtable.ts
+++ airtable.ts
@@ -1,2 +1,20 @@
 import { config } from "dotenv";
 config();
+
+/**
+ * This is NOT the API key, it's the public app ID
+ */
+const AIRTABLE_APP_ID = "appoaPyQSLrreakJN";
+
+import Airtable from "airtable";
+const airtable = new Airtable();
+const base = airtable.base(AIRTABLE_APP_ID);
+
+const log = (...args: string[]) =>
+  console.log(new Date().toISOString(), ...args);
+
+const update = async () => {
+  log("Updating data from Airtable");
+};
+
+update();
