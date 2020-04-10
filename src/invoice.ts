c30045d73366eaa376227b4bff6cbeebf1168332
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-10 18:48:59 +0530

    :goal_net: Make sure required data exists

--- src/invoice.ts
+++ src/invoice.ts
@@ -7,10 +7,13 @@ interface Record {
   _id: "string";
   amount: number;
   method: "Cash" | "NEFT" | "IMPS" | "TPT" | "UPI" | "Cheque";
-  mobile: string;
   name: string;
   status: string;
   toAccount: string;
+  address?: string;
+  mobile?: string;
+  panNo?: string;
+  utrPaymentDetailsImpsNeftRtgsNo?: string;
 }
 
 export const createInvoices = async () => {
@@ -20,14 +23,32 @@ export const createInvoices = async () => {
     completedInvoiceStep: string;
     sentInvoiceStep: string;
   } = safeLoad(yaml);
-  log("Generating invoices");
+  log("\n\nGenerating invoices");
 
   const json: Record[] = await readJson(join(".", fileName("Donations")));
   const recordsToGenerate = json.filter(
     i => i.status === sheetFile.generateInvoiceStep
   );
-
   log(recordsToGenerate.length, "records to generate invoice for");
+
+  for await (const record of recordsToGenerate) {
+    try {
+      await createSingleInvoice(record);
+    } catch (error) {
+      log("ERROR", error.toString() + "\n");
+    }
+  }
+};
+
+export const createSingleInvoice = async (record: Record) => {
+  log("Generating invoice for record", record._id, record.name);
+
+  if (!record.address) throw new Error("Address not available");
+  if (!record.amount) throw new Error("Amount not available");
+  if (!record.mobile) throw new Error("Phone number not available");
+  if (!record.panNo) throw new Error("PAN not available");
+
+  log("Successfully generated\n");
 };
 
 createInvoices();
